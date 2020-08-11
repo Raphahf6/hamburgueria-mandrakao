91908bf989f1805e781371cb1d5a54f630143d19
raphahf6 <raphaellima6@gmail.com>
2020-08-11 01:50:32 -0300

    aula dia 11/08

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 window.onload = function () {
+    let btnCarrinho = document.getElementById('btn-carrinho')
+
 
     let carrinho = {
-        produtos: []
+        produtos: [],
+        total: 0
     }
 
-    class Produto {
+    class ProdutoNoCatalogo {
         constructor(nome, img, preco, botaoComprar, inputQuantidade) {
             this.nome = nome
             this.img = img
@@ -17,75 +20,90 @@ window.onload = function () {
 
     }
 
-    let visualizarCarrinho = document.getElementById('btn-carrinho')
 
-    let uva = new Produto('Uva', document.getElementById('imgUva'), document.getElementById('precoUva'), document.getElementById('comprarUva'), document.getElementById('quantidadeUva'))
-    let uvaSemCaroco = new Produto('Uva sem caroço', document.getElementById('imgUvaSemCaroco'), document.getElementById('precoUvaSemCaroco'), document.getElementById('comprarUvaSemCaroco'), document.getElementById('quantidadeUvaSemCaroco'))
-    let maca = new Produto('Maça', document.getElementById('imgMaca'), document.getElementById('precoMaca'), document.getElementById('comprarMaca'), document.getElementById('quantidadeMaca'))
+    class ProdutoNoCarrinho {
+        constructor(nome, quantidade, preco) {
+            this.nome = nome
+            this.quantidade = quantidade
+            this.preco = preco
+        }
+    }
+
+
+    let uva = new ProdutoNoCatalogo('Uva', document.getElementById('imgUva'), document.getElementById('precoUva'), document.getElementById('comprarUva'), document.getElementById('quantidadeUva'))
+    let uvaSemCaroco = new ProdutoNoCatalogo('Uva sem caroço', document.getElementById('imgUvaSemCaroco'), document.getElementById('precoUvaSemCaroco'), document.getElementById('comprarUvaSemCaroco'), document.getElementById('quantidadeUvaSemCaroco'))
+    let maca = new ProdutoNoCatalogo('Maça', document.getElementById('imgMaca'), document.getElementById('precoMaca'), document.getElementById('comprarMaca'), document.getElementById('quantidadeMaca'))
 
 
 
     uva.comprar.addEventListener('click', () => {
-        let preco = uva.quantidade.value * uva.preco.value
-        alert(`Total: ${preco}`)
-        carrinho.produtos.push(ProdutoUva = {
-            nome: uva.nome,
-            preco: preco,
-            quantidade: uva.quantidade.value
-        })
-
-        alert(`O item ${uva.nome} foi adicionado ao carrinho`)
-
+        adicionaProdutoAoCarrinho(uva)
     })
 
+
+
     uvaSemCaroco.comprar.addEventListener('click', () => {
-        let preco = uvaSemCaroco.quantidade.value * uvaSemCaroco.preco.value
-        carrinho.produtos.push(ProdutoUvaSemCaroco = {
-            nome: uvaSemCaroco.nome,
-            preco: preco,
-            quantidade: uvaSemCaroco.quantidade.value
-        })
-        alert(`O item ${uvaSemCaroco.nome} foi adicionado ao carrinho`)
+        adicionaProdutoAoCarrinho(uvaSemCaroco)
+
     })
 
     maca.comprar.addEventListener('click', () => {
-        let preco = maca.quantidade.value * maca.preco.value
-        carrinho.produtos.push(ProdutoMaca = {
-            nome: maca.nome,
-            preco: preco,
-            quantidade: maca.quantidade.value
-        })
-        alert(`O item ${maca.nome} foi adicionado ao carrinho`)
-    })
-
-    visualizarCarrinho.addEventListener('click', () => {
-        for (i = 0; i < carrinho.produtos.length; i++) {
-            alert(`Produto ${carrinho.produtos[i].nome} Quantidade: ${carrinho.produtos[i].quantidade} Preco: ${carrinho.produtos[i].preco}`)
-            
-        }
+        adicionaProdutoAoCarrinho(maca)
 
     })
 
+    btnCarrinho.addEventListener('click', () => {
+        visualizarCarrinho()
+    })
 
-    criarNovoProduto = (nome, img, preco) => {
-        const novoProduto = new Produto(nome, img, preco)
 
+    criarNovoProdutoNoCatalogo = (nome, imgUrl, preco) => {
+
+        const novoProduto = new Produto(nome, imgUrl, preco)
         const container = document.querySelector('.row')
         const divElement = document.createElement('div').className('col-sm-12 col-lg-4')
+
         const cardElement = document.createElement('div').className('card')
-        const imgCard = document.createElement('img').className('card-img-top')
         const cardBody = document.createElement('div').className('card-body')
-        const nomeProduto = document.createElement('h5').className('card-title')
         const cardText = document.createElement('p').className('card-text')
+        const imgCard = document.createElement('img').className('card-img-top')
+
+        const nomeProduto = document.createElement('h5').className('card-title')
         const inputPreco = document.createElement('input').id(`preco${nome}`)
         const inputQuantidade = document.createElement('input').className(`${nome}Quantidade`)
+        
 
-        imgCard.setAttribute("src", novoProduto.img)
+        imgCard.setAttribute("src", novoProduto.imgUrl)
         nomeProduto.textContent = novoProduto.nome
         cardText.textContent = 'Preco:'
         inputPreco.setAttribute('value', preco)
 
+        divElement.appendChild(cardElement)
+        divElement.appendChild(cardBody)
+        divElement.appendChild(cardText)
 
+
+    }
+
+    visualizarCarrinho = () => {
+        for (i = 0; i < carrinho.produtos.length; i++) {
+            let produtoAtual = carrinho.produtos[i]
+            carrinho.total += produtoAtual.preco
+            alert(`Produto: ${produtoAtual.nome} `)
+        }
+        alert(`Total: ${carrinho.total} reais`)
+    }
+
+    adicionaProdutoAoCarrinho = (produto) => {
+
+        if(produto.quantidade.value && produto.preco.value){
+            let preco = produto.quantidade.value * produto.preco.value
+            carrinho.produtos.push(produto = new ProdutoNoCarrinho(produto.nome, produto.quantidade.value, preco))
+            alert(`O item ${produto.nome}, Quantidade: ${produto.quantidade} foi adicionado ao carrinho`)
+        } else{
+            alert(`Selecione a quantidade de produto que deseja primeiro`)
+        }
+        
     }
 
 }
